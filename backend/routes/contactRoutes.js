784190ad9bd98bdac4acd5169f7a5042d7d72c21
Naftali <163560278+23008024@users.noreply.github.com>
2026-06-24 13:01:27 +0200

    Update contactRoutes.js

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -3,16 +3,27 @@ const router = express.Router();
 
 const nodemailer = require("nodemailer");
 
-
 const transporter = nodemailer.createTransport({
 
-    service: "gmail",
+    host: "smtp.gmail.com",
+
+    port: 587,
+
+    secure: false,
+
+    family: 4,
 
     auth: {
 
         user: process.env.EMAIL_USER,
 
         pass: process.env.EMAIL_PASS
+
+    },
+
+    tls: {
+
+        rejectUnauthorized: false
 
     }
 
